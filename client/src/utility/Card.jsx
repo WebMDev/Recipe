8a704b5989bd6d32fcdfb78a466232bf4a13ac40
WebMDev <manojkumarkumawat001@gmail.com>
2025-07-10 14:16:04 +0530

    Fixed case-sensitive filename issue for image name

--- client/src/utility/Card.jsx
+++ client/src/utility/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 // import img from "../assets/recipe/recipe-2-550x690.jpg";
-import STAR from "../assets/svgImg/star.svg";
-import Heart from "../assets/svgImg/heart.svg";
-import Bookmark from "../assets/svgImg/bookmark.svg";
-import Watch from "../assets/svgImg/watch.svg";
-import ct1 from "../assets/svgImg/ct-1.svg";
-import Cheaf from "../assets/svgImg/cheaf.svg";
+// import STAR from "../assets/svgImg/star.svg";
+// import Heart from "../assets/svgImg/heart.svg";
+// import Bookmark from "../assets/svgImg/bookmark.svg";
+// import Watch from "../assets/svgImg/watch.svg";
+// import ct1 from "../assets/svgImg/ct-1.svg";
+// import Cheaf from "../assets/svgImg/cheaf.svg";
 import {Infocard} from "../assets/contant";
 
 
@@ -19,14 +19,14 @@ const Card = ({ img, desc , title , style}) => {
             <ul className="w-full pt-2 flex justify-between items-start absolute top-[0.5rem] left-[1rem]">
 
                 <li className="flex items-center text-[14px] p-[5px_10px] rounded-3xl bg-white">
-                    <img src={STAR} alt="" />
+                    {/* <img src={STAR} alt="" /> */}
                     <span className="pl-2 font-[500]">4.8</span>
                 </li>
 
                 <li className="relative flex flex-col gap-4 pr-7">
-                    <img src={Heart} className=" h-[35px] w-[35px] bg-white p-[6px] rounded-[50%]" alt="" />
+                    {/* <img src={Heart} className=" h-[35px] w-[35px] bg-white p-[6px] rounded-[50%]" alt="" /> */}
                     <span className="p-[8px] bg-white rounded-[50%]">
-                        <img src={Bookmark} className=" h-[20px] w-[20px]" alt="" />
+                        {/* <img src={Bookmark} className=" h-[20px] w-[20px]" alt="" /> */}
                     </span>
                 </li>
 
@@ -43,17 +43,17 @@ const Card = ({ img, desc , title , style}) => {
                             <ul className="flex justify-start items-center gap-5 pt-[15px]">
 
                                 <li className="flex justify-center items-center text-[#A1A1A1] gap-2">
-                                    <img src={Watch} alt="" className="text-[#A1A1A1]"/>
+                                    {/* <img src={Watch} alt="" className="text-[#A1A1A1]"/> */}
                                     <span className="text-[14px]">5 min</span>
                                 </li>
 
                                 <li className="flex justify-center items-center text-[#A1A1A1] gap-2">
-                                    <img src={ct1} className="h-[17px] text-[#A1A1A1]" alt=""/>
+                                    {/* <img src={ct1} className="h-[17px] text-[#A1A1A1]" alt=""/> */}
                                     <span className="text-[14px]">Lebanese</span>
                                 </li>
 
                                 <li className="flex justify-center items-center text-[#A1A1A1] gap-2">
-                                    <img src={Cheaf} alt="" className="text-[#A1A1A1]"/>
+                                    {/* <img src={Cheaf} alt="" className="text-[#A1A1A1]"/> */}
                                     <span className="text-[14px]">Beginner</span>
                                 </li>
                             </ul>
